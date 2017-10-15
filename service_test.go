5840b7282603ea3af774c413ed39f5a26e3e0347
liang <liyuliang2013@gmail.com>
2017-10-16 00:08:07 +0800

    add usage testing file

--- service_test.go
+++ service_test.go
@@ -1,7 +1,33 @@
 package services
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestService(t *testing.T) {
-	
+
+	AddMultiProcessTask("test println hello", func(workerNum int) (err error) {
+		println("hello ~")
+
+		return err
+	})
+	AddMultiProcessTask("test println world", func(workerNum int) (err error) {
+		println("world ~")
+
+		return err
+	})
+
+	AddSingleProcessTask("test pringln --- ", func(workerNum int) (err error) {
+		println("---")
+
+		return err
+	})
+
+	// set up 5 goroutine to run
+	Service().SetWorkerNum(5).Start()
+
+
+	// after 3 second, all goroutine will stop
+	time.Sleep(time.Second * 3)
 }
