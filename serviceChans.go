250c533c2013101e450e1bed42f77b477e47574e
liyuliang <liyuliang2013@gmail.com>
2018-10-07 00:17:45 +0800

    add names method

--- serviceChans.go
+++ serviceChans.go
@@ -24,6 +24,13 @@ func ChanList() *chanList {
 
 	return list
 }
+func (list *chanList) Names() []string {
+	var names []string
+	for name, _ := range list.chans {
+		names = append(names, name)
+	}
+	return names
+}
 
 func (list *chanList) Register(name string, registerChan interface{}) {
 
